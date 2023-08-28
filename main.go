31d439563ceee460569a3f45224b58a49eb07c9a
Bhagirath Saxena <72344025+rix4uni@users.noreply.github.com>
2023-08-28 07:44:14 +0530

    Update main.go

--- main.go
+++ main.go
@@ -10,18 +10,20 @@ import (
 
 func main() {
 	var quietMode bool
-	var dryRun bool
 	var trim bool
+	var printMode bool
+	var appendMode bool
 	flag.BoolVar(&quietMode, "q", false, "quiet mode (no output at all)")
-	flag.BoolVar(&dryRun, "d", false, "don't append anything to the file, just print the new lines to stdout")
 	flag.BoolVar(&trim, "t", false, "trim leading and trailing whitespace before comparison")
+	flag.BoolVar(&printMode, "p", false, "print only unique input directly to stdout")
+	flag.BoolVar(&appendMode, "a", false, "append output in a file and print in teminal")
 	flag.Parse()
 
 	fn := flag.Arg(0)
 
 	lines := make(map[string]struct{}) // Use struct{} for values to save space
 
-	if fn != "" {
+	if fn != "" && !appendMode {
 		// read the whole file into a map if it exists
 		r, err := os.Open(fn)
 		if err == nil {
@@ -36,37 +38,73 @@ func main() {
 			}
 			r.Close()
 		}
+	}
 
-		if !dryRun {
-			// re-open the file for appending new stuff
-			var err error
-			f, err := os.OpenFile(fn, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "failed to open file for writing: %s\n", err)
-				return
+	if appendMode {
+		// open the file for appending new stuff
+		f, err := os.OpenFile(fn, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to open file for writing: %s\n", err)
+			return
+		}
+		defer f.Close()
+
+		// Create a buffered writer to minimize the number of write system calls
+		w := bufio.NewWriter(f)
+		defer w.Flush()
+
+		sc := bufio.NewScanner(os.Stdin)
+		for sc.Scan() {
+			line := sc.Text()
+			if trim {
+				line = strings.TrimSpace(line)
 			}
-			defer f.Close()
+			fmt.Println(line) // Printing line to stdout
+			w.WriteString(line + "\n") // Appending line to file
+		}
+		return
+	}
 
-			// Create a buffered writer to minimize the number of write system calls
-			w := bufio.NewWriter(f)
-			defer w.Flush()
+	if printMode {
+		sc := bufio.NewScanner(os.Stdin)
+		for sc.Scan() {
+			line := sc.Text()
+			if trim {
+				line = strings.TrimSpace(line)
+			}
+			if _, exists := lines[line]; !exists {
+				lines[line] = struct{}{}
+				fmt.Println(line)
+			}
+		}
+		return // exit the program since we've done what was required by the -p flag
+	}
 
-			sc := bufio.NewScanner(os.Stdin)
-			for sc.Scan() {
-				line := sc.Text()
-				if trim {
-					line = strings.TrimSpace(line)
+	if fn != "" {
+		// re-open the file for appending new stuff
+		f, err := os.OpenFile(fn, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to open file for writing: %s\n", err)
+			return
+		}
+		defer f.Close()
+
+		// Create a buffered writer to minimize the number of write system calls
+		w := bufio.NewWriter(f)
+		defer w.Flush()
+
+		sc := bufio.NewScanner(os.Stdin)
+		for sc.Scan() {
+			line := sc.Text()
+			if trim {
+				line = strings.TrimSpace(line)
+			}
+			if _, exists := lines[line]; !exists {
+				lines[line] = struct{}{}
+				if !quietMode {
+					fmt.Println(line)
 				}
-
-				if _, exists := lines[line]; !exists {
-					lines[line] = struct{}{}
-					if !quietMode {
-						fmt.Println(line)
-					}
-					if !dryRun && fn != "" {
-						w.WriteString(line + "\n")
-					}
-				}
+				w.WriteString(line + "\n")
 			}
 		}
 	}
